aa9730be2a07c0262bfc72bc1465e079d5019a57
Atyam Sriharsha <atyamsriharsha@gmail.com>
2015-07-12 01:36:23 +0530

    My Uva solutions

---
+++ eventuallyperiodicsequence.cpp
@@ -0,0 +1,191 @@
+/* author :: atyam*/
+#include<bits/stdc++.h>
+
+using namespace std;
+
+/*typedef's for frequently used*/
+
+typedef long long ll;
+typedef vector<int>VI;
+typedef vector<ll>VLI;
+typedef pair<int,int> ipair;
+typedef pair<ll,ll> lpair;
+typedef unsigned long long int ull;
+
+/*debugging*/
+
+#define out1(x)cout<<#x<<" is "<<x<<endl
+#define out2(x,y)cout<<#x<<" is "<<x<<" "<<#y <<" is "<<y<<endl
+#define out3(x,y,z)cout<<#x<<" is "<<x<<" "<<#y<<" is "<<y<<" "<<#z<<" is "<<z<<endl;
+#define out4(a,b,c,d)cout<<#a<<" is "<<a<<" "<<#b<<"  is "<<b<<" "<<#c<<" is "<<c<<" "<<#d<<" is "<<d<<endl;
+#define show(i,a,n) for(i=0;i<n;i++)cout<<a[i]<<" ";cout<<endl;
+#define sz(a) cout<<"size of "<<#a<<" is "<<a.size()
+#define exectime() cout<<"execution time "<<(double)(clock() - tStart)/CLOCKS_PER_SEC<<endl;
+
+/*standard values*/
+
+#define INF 1e18
+#define PI 3.14159265359
+
+/*For variables*/	
+
+#define LET(a) __typeof(a) x(a) 
+#define ALL(a) a.begin(),a.end()
+
+/*Frequent functions*/
+
+#define PB push_back
+#define MP make_pair
+#define F first
+#define S second
+#define si(n) scanf("%d",&n)
+#define sl(n) scanf("%lld",&n)
+#define pu putchar
+#define gu getchar
+#define FastIn std::ios::sync_with_stdio(false);cin.tie(NULL);cout.tie(NULL)
+#define swap(VAR1,VAR2) VAR1=VAR1^VAR2;VAR2=VAR1^VAR2;VAR1=VAR1^VAR2
+#define UB(x,a) (upper_bound(ALL(a),x)-a.begin())
+#define LB(a,x) (lower_bound(ALL(a),x)-a.begin())
+#define NP(a) next_permutation(ALL(a))
+
+/*exponentiation*/
+
+
+ll inline ipow(ll a,ll b,ll m){ll val=1;a%=m;while(b){if(b&01)val=(val*a)%m;b>>=1;a=(a*a)%m;};return val%m;}
+ll inline ipow(ll a,ll b){ll val=1;while(b){if(b&01)val=(val*a);b>>=1;a=(a*a);};return val;}
+
+/* Seive */
+
+
+/*
+#define LIM1 1000001
+bitset<LIM1> isp;
+vi primes;
+inline void seive(){isp.set();isp.reset(0);isp.reset(1);for(int i=2;i*i<LIM1;i++)if(isp[i]){for(int j=i<<1;j<LIM1;j+=i)isp.reset(j);}repl(i,2,LIM1)if(isp[i]){primes.pb(i);}}
+*/
+
+/* Totient */
+
+/*
+#define LIM4 1000001
+int phi[LIM4];
+void totient(){repl(i,1,LIM4)phi[i]=i;tr(primes,it)for(int i=*it;i<LIM4;i+=*it)phi[i]-=phi[i]/(*it);}
+*/
+
+/* Inverse Modulo */
+
+/*inline ll invmod(ll a, ll b){return (isp[b])?ipow(a,b-2,b):ipow(a,phi[b]-1,b);}*/
+
+/* Combinatorics */
+
+/*
+#define LIM2 1000001
+ll fact[LIM2];
+inline void Fact(){fact[0]=1;repl(i,1,LIM)fact[i]=(i*fact[i-1])%MOD;}
+inline ll inv(ll x,ll mod){return ipow(x,mod-2,mod);}
+inline ll comb(ll a, ll b){return ((fact[a]*inv(fact[b],MOD)%MOD)*inv(fact[a-b],MOD))%MOD;}
+*/
+
+/*loops and initialisation*/
+
+#define init(a,b) memset(a,b,sizeof(a))
+#define rep(i,a,b) for(ll i=a;i<=b;i++)
+#define repi(i,a,b) for(ll i=a;i>=b;i--)
+#define repdf(i,a,b,d) for(ll i=a;i<=b;i+=d)
+#define repdb(i,a,b,d) for(ll i=a;i>=b;i-=d)
+#define repl(i,a,b) for (ll i=a;i<b;++i)
+#define repil(i,a,b) for (ll i=a;i>b;i--)
+#define tr(v,it) for( LET(it,v.begin()) ; it != v.end() ; it++)
+#define rtr(v,it) for( LET(it,v.rbegin()) ; it != v.rend() ; it++)
+#define TC() int t;cin>>t;while(t--)
+
+/*space for global variables*/
+
+set<ipair> s;
+set<int> xc;
+set<int> yc;
+int dx[4] ={-1,1,0,0} ;
+int dy[4] ={0,0,1,-1} ;
+
+/**************************************************************  	
+  	          Main Code
+**************************************************************/
+vector<string> func;
+string str;
+long long N, n;
+
+ll f( ll x)
+{
+	stack<ll> s;
+	ll a,b ;		
+	repl(i,0,(long long int)func.size())
+	{		
+		if(func[i] == "x")
+		{
+			s.push( x);
+		} 
+		else if( func[i] == "N")
+		{
+			s.push( N);
+		} 
+		else if( func[i] == "+" || func[i] == "*" || func[i] == "%") 
+		{	
+			b = s.top(); 
+			s.pop();
+			a = s.top(); 
+			s.pop();						
+			if(func[i] == "+") 
+				a += b; 
+			if(func[i] == "*") 
+				a *= b;
+			if(func[i] == "%") 
+				a %= b;	
+			a %= N;	
+			s.push(a);
+		} 
+		else
+		{
+			ll a; sscanf(func[i].c_str(), "%lld", &a);
+			s.push(a);
+		}
+	}	
+	return (s.top() % N);
+}
+
+
+int main()
+{	
+	while(1)
+	{
+		cin >> N >> n ;
+		if(N == 0)
+		 break;
+		func.clear();
+		getline(cin, str);
+		stringstream ss(str);
+		while( ss >> str)
+		{
+			func.PB(str);			
+		}		
+		ll tort = f(n), hare = f(f(n));
+		while(tort != hare)
+		{ 
+			tort = f(tort); 
+			hare = f(f(hare)); 
+		}
+		hare = n;
+		while(tort != hare)
+		{ 
+			tort = f(tort); 
+			hare = f(hare); 
+		}
+		ll lamb = 1; hare = f(tort);
+		while(tort != hare)
+		{ 
+			hare = f(hare); 
+			lamb++; 
+		}
+		printf("%lld\n", lamb);
+	}
+	return 0;
+}
